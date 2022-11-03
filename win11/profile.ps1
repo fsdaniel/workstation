225cb03e1220ec22c9dd1b5f4e570aa6799c7811
Daniel Koopmans <daniel.koopmans@gmail.com>
2022-11-03 01:08:58 +0100

    Update profile.ps1

--- win11/profile.ps1
+++ win11/profile.ps1
@@ -2,12 +2,12 @@ $Env:Path += ';%USERPROFILE%\.krew\bin'
 $MyStartFolder = 'c:\k8s-tools\'
 if (-not( Test-Path -Path $MyStartFolder)) {
     mkdir c:\k8s-tools\
-}
+}    
 if (-not(Test-Path -Path 'c:\k8s-tools\k-alias.ps1')) {
-    curl https://raw.githubusercontent.com/fsdaniel/workstation/main/win11/k-alias.ps1 -o k-alias.ps1
+    curl https://raw.githubusercontent.com/fsdaniel/workstation/main/win11/k-alias.ps1 -o c:\k8s-tools\k-alias.ps1
 }
 if (-not(Test-Path -Path 'c:\k8s-tools\prompt.json')) {
-    curl https://raw.githubusercontent.com/fsdaniel/workstation/main/win11/prompt.json -o prompt.json
+    curl https://raw.githubusercontent.com/fsdaniel/workstation/main/win11/prompt.json -o c:\k8s-tools\prompt.json
 }
 cat k-alias.ps1 | Out-String | Invoke-Expression
 oh-my-posh init pwsh --config 'c:\k8s-tools\prompt.json' | Invoke-Expression
